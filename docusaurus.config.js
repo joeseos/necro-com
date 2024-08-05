571d7ffbfd7efe47a404b55af236e791b7d6101b
Johan Jönsson <jonsson.johan@outlook.com>
2024-08-05 12:53:34 +0200

    Update docusaurus.config.js
    Change to set darkmode to default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,6 +136,14 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+// Set darkmode to default
+  export default {
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
+  },
 };
-
 export default config;
