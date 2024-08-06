cbe421aabe476aa029ce5cadb6053f5bbe237e66
Johan Jönsson <jonsson.johan@outlook.com>
2024-08-06 11:56:42 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'NECRO-COM',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Want to find something quickly? Use the search.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
