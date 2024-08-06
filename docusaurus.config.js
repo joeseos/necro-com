ea1fba3b208009ca70e605d24be47cd57a685a8e
Johan Jönsson <jonsson.johan@outlook.com>
2024-08-06 11:46:33 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/joeseos/docusaurus-joesoes-se/tree/main/docs',
+            'https://github.com/joeseos/docusaurus-joesoes-se',
         },
         theme: {
           customCss: './src/css/custom.css',
