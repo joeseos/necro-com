fbaa9452a83a5c241112c8969a01fabfb906ecbb
Johan Jönsson <jonsson.johan@outlook.com>
2024-09-19 15:31:49 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,45 +5,31 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+        <Heading as="h1" className="herotitle">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="herosubtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            CORE RULES
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            style={{ marginLeft: '10px' }}
-            to="/docs/gangs">
-            GANGS
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            style={{ marginLeft: '10px' }}
-            to="/docs/gang-fighters--their-weaponry/weapon-traits">
-            TRADING POST
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
     </header>
   );
 }
-
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={${siteConfig.title}}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
