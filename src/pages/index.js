7cdc6bd091c896a41c105ccf03f259d109dc7a94
Johan Jönsson <jonsson.johan@outlook.com>
2024-09-19 14:59:35 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
@@ -37,5 +36,19 @@ function HomepageHeader() {
         </div>
       </div>
     </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={${siteConfig.title}}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
   );
 }
